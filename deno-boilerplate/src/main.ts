a3d11c62e1e371c30b1a3f34a284a8cfe4965d64
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-08-12 17:54:21 -0400

    this is not working

--- deno-boilerplate/src/main.ts
+++ deno-boilerplate/src/main.ts
@@ -1,6 +1,18 @@
-import { log } from "./log.ts";
+import {log} from "./log.ts";
+import {Application, Router} from 'https://deno.land/x/oak@v4.0.0/mod.ts';
 
-const main = () => {
+const main = async () => {
+  const app = new Application();
+  const router = new Router();
+
+  router.get('/', context => {
+    context.response.body = 'Hello world!';
+
+  });
+
+  app.use(router.routes());
+
+  await app.listen({port: 8000});
   log.info("Starting your Deno App");
 };
 
