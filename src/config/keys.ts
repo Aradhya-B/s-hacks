92bd93eae791d46cdbeb39de821e9f6f0cd1c32f
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-08-13 18:15:26 -0400

    Add ocr keys

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -3,3 +3,5 @@ export const IMAP_USER = 'scotiabank.alakazam.abansal@gmail.com';
 export const IMAP_PASSWORD = 'scotiabank';
 export const IMAP_HOST = 'imap.gmail.com';
 export const IMAP_PORT = 993;
+export const COMPUTER_VISION_SUBSCRIPTION_KEY = 'c1d8c433cc524569a5f2fbe0fa515058';
+export const COMPUTER_VISION_ENDPOINT = 'https://s-hacks-2020-computer-vision.cognitiveservices.azure.com/';
--- src/routes/bills.ts
+++ src/routes/bills.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import {COMPUTER_VISION_SUBSCRIPTION_KEY, COMPUTER_VISION_ENDPOINT} from '../config/keys';
 
 const router = express.Router();
 
 router.get('/', () => {
-    console.log("bills");
+  console.log("bills");
+})
+
+router.post('/upload', (req, res) => {
+
 })
 
 export default router;
