152bb625b2daf34c5b5b141dc643088dd2a70346
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-08-12 22:54:34 -0400

    Connect mongoose

---
+++ src/config/keys.ts
@@ -0,0 +1,1 @@
+export const MONGODB_URI = 'mongodb+srv://shacks:scotiabank@shacks.uapy5.mongodb.net/shacks?retryWrites=true&w=majority';
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,34 @@
 import express from 'express';
-import cors from 'cors'; 
+import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import mongoose from 'mongoose'; 
-import bodyParser from 'body-parser'; 
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import routes from './routes/index';
+import {MONGODB_URI} from './config/keys';
 
-const PORT = process.env.PORT || 8000; 
-
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
 // Middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 app.use(morgan('combined'));
 app.use(helmet());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use("", routes);
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => console.log('DB is connected'),
+    (err: any) => console.log('DB connection failed')
+  );
+
 app.listen(PORT, () => console.log("listening on port: " + PORT.toString()));
+
