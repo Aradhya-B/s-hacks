af1b1a2e180ac4b72c836c1aaed4df996db9bd15
Matthew <matthewdanics@gmail.com>
2020-08-13 22:57:12 -0400

    stuff

--- src/routes/bills.ts
+++ src/routes/bills.ts
@@ -2,16 +2,30 @@ import express from 'express';
 import axios from 'axios';
 import request from 'request';
 import {COMPUTER_VISION_SUBSCRIPTION_KEY, COMPUTER_VISION_ENDPOINT} from '../config/keys';
+import * as vision from '@google-cloud/vision';
 const uriBase = COMPUTER_VISION_ENDPOINT + 'vision/v3.0/ocr';
-
 const router = express.Router();
 
 router.get('/', () => {
   console.log("bills");
+
 })
 
-router.post('/upload', (req, res) => {
-  const bin = req.body.bin;
+
+router.get('/upload', async (req, res) => {
+    console.log("starting");
+    const client = new vision.ImageAnnotatorClient();
+
+    const [result] = await client.textDetection("https://i.ibb.co/F0Y284J/IMG-20200813-213630.jpg");
+    const detections = result.textAnnotations;
+    console.log('Text:');
+    detections.forEach(text => console.log(text));
+})
+
+
+router.get('/upload_old', (req, res) => {
+  console.log("uploading..");
+//   const bin = req.body.bin;
   const params = {
     'language': 'unk',
     'detectOrientation': 'false'
@@ -19,12 +33,16 @@ router.post('/upload', (req, res) => {
   const options = {
     uri: uriBase,
     qs: params,
-    body: '{"data": ' + '"' + bin + '"}',
+    body: '{"url": "https://i.ibb.co/F0Y284J/IMG-20200813-213630.jpg"}',
     headers: {
-      'Content-Type': 'application/octet-stream',
-      'Ocp-Apim-Subsrciption-Key': COMPUTER_VISION_SUBSCRIPTION_KEY
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': COMPUTER_VISION_SUBSCRIPTION_KEY
     },
   };
+
+  console.log("data:-----");
+//   console.log(bin);
+
   request.post(options, (error, response, body) => {
     if (error) {
       console.log('Error: ', error);
