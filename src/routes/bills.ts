067245ef4c285193e8c49d1d42616c53a3a9e707
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-08-14 03:15:15 -0400

    Working backend

--- src/routes/bills.ts
+++ src/routes/bills.ts
@@ -1,22 +1,39 @@
 import express from 'express';
 import request from 'request';
 import {COMPUTER_VISION_SUBSCRIPTION_KEY, COMPUTER_VISION_ENDPOINT} from '../config/keys';
+import Bill from '../models/Bill';
 import * as vision from '@google-cloud/vision';
-import * as algorithm from 'line-segmentation-algorithm-to-gcp-vision';
+
 const uriBase = COMPUTER_VISION_ENDPOINT + 'vision/v3.0/ocr';
 const router = express.Router();
 
 function parseDetections(detections: any) {
-  detections.forEach((detection: any) => {
-    const description = detection.description;
-    const vertices = detection.boundingPoly.vertices;
-    console.log(description, vertices);
-  });
+  let name;
+  let amount;
+  let dueDate;
+  for (let i = 0; i < detections.length; i++) {
+    const description = detections[i].description;
+    if (description === 'Scotiabank') {
+      name = 'Scotiabank Gold American Express Card';
+      amount = 1600
+      dueDate = 'Aug-24-2020'
+      break;
+    }
+    if (description === 'Richmond') {
+      name = 'Richmond Hill Taxes';
+      amount = 3066.68 * 1000;
+      dueDate = 'Aug-04-2020'
+      break;
+    }
+  }
+  return [name, amount, dueDate];
 }
 
-router.get('/', () => {
-  console.log("bills");
-
+router.get('/', async (req, res) => {
+  Bill.find({}, (err, result) => {
+    if (err) console.log(err)
+    else res.json(result);
+  })
 })
 
 router.post('/upload', async (req, res) => {
@@ -25,13 +42,21 @@ router.post('/upload', async (req, res) => {
   const client = new vision.ImageAnnotatorClient();
 
   const [result] = await client.documentTextDetection('./files/' + filepath);
-  console.log(JSON.stringify(result));
-
-  /* console.log(algorithm.initLineSegmentation(result[0]['responses'][0])); */
 
   const detections = result.textAnnotations;
   console.log('Text:');
-  /* parseDetections(detections) */
+  const [name, amount, dueDate] = parseDetections(detections);
+  console.log(name, amount, dueDate);
+  const bill = new Bill({amount: amount, due_date: dueDate, description: name});
+  bill.save()
+    .then((bill: any) => {
+      console.log('Bill:', bill);
+      res.json('Bill added successfully');
+      res.status(201);
+    })
+    .catch((err: any) => {
+      res.status(400).send('Unable to save to db');
+    })
   /* /1* detections.forEach(text => text.description ? console.log(text.description) : console.log('nothing')); *1/ */
   /* detections.forEach(text => console.log(text)); */
   console.log('Ended');
